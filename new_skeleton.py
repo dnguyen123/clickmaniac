b2323bb3357a59f39cff560504861d7a11b3183c
Kelly Woo <kellywoo@Kellys-MacBook-Air-2.local>
2018-03-06 21:20:19 -0800

    adsjfk

--- new_skeleton.py
+++ new_skeleton.py
@@ -6,11 +6,12 @@ from facebookads import adobjects
 from facebookads.adobjects.adaccountuser import AdAccountUser
 from facebookads.adobjects.campaign import Campaign
 from facebookads.adobjects.adset import AdSet
+from facebookads.adobjects.targeting import Targeting
 
 my_app_id = '299363293779254'
 my_app_secret = '3ec97126104c50bed63580b6968659fa'
 # my_access_token = 'EAAEQRPLI1TYBABYxJaOLZCBNcBWH98EtWOXTnF8VZCd4Bk1FhHn0cWrFqXMypG4T18A6Vsv40AJniyQhHt7uLxDgIe04dRxonm54ULJQY8KoWVZBr6hZBaPTpO2PcX9G5klLzqx2HATdrvZCJv2C5FsKP82juToyUS6VpZA6j73gZDZD'
-my_access_token='EAAEQRPLI1TYBAKhPMqdCjCMZAJ5qNa0CqHcSabuOSfjw3yHlKk2mPYxhxppkDprf2LQ0LQm1TolETL50t3dDvxp1Dk2sEIDHy4OZCexyFlKZAcrv6ZCBPuUKnmudbxUrG0zNuCZChhp4JZCGZAPHEZAaRZCZA0iBlBqb8CFfWQz6gmBZAPDRjl8glOoSZAIzgT0dD5ZBMHXJNrDZBSMQZDZD'
+my_access_token='EAAEQRPLI1TYBALZBWA0MPO1miFhRtQRMxTdlaXClheN5nGZBmIDRne6hDVlfSGA6dwD74P4B4vEbQDcAF6rqbdJJrzyxA8rrqiKaqLQbmePvcVKd0xGjO1o9D9lIqTIFiLOi4Wo7MHYulJnCiOwcc4JX6iAt3NMY1B0kFlvSH1BFmuP2appWOqYsGAy2tVCHRnpBIqfKxJnpKY5DizzKhYExtZBCj0cV7q1VIaIywZDZD'
 my_campaign_id = '23842782742530444'
 my_ad_account_id = 'act_356288071441451'
 my_page_id = '103185246428488'
@@ -45,6 +46,40 @@ def create_new_ad_set():
     """
     Create and update your ad set here
     """
+#https://developers.facebook.com/docs/marketing-api/reference/ad-campaign
+    params = {
+        AdSet.Field.name: 'My Ad Set',
+        AdSet.Field.campaign_id: my_campaign_id,
+        AdSet.Field.is_autobid: TRUE,
+        AdSet.Field.daily_budget: 2,
+        AdSet.Field.billing_event: AdSet.BillingEvent.impressions,
+        AdSet.Field.optimization_goal: AdSet.OptimizationGoal.page_likes,
+# https://developers.facebook.com/docs/marketing-api/targeting-specs
+        AdSet.Field.targeting: {
+            Targeting.Field.geo_locations: {
+                'countries': ['US'],
+            },
+            Targeting.Field.age_min: 25,
+            Targeting.Field.age_max: 30,
+            Targeting.Field.flexible_spec: {
+            
+                'interests': [
+                    {
+                        'id': ,
+                        'name': ,
+                    },
+                    {
+                        'id': ,
+                        'name': ,
+                    },
+                ],
+                'interested_in': []
+            },
+        },
+        AdSet.Field.status: AdSet.Status.active,
+    }
+    adset = adset.create_ad_set(params=params)
+
     return adset
 
             
